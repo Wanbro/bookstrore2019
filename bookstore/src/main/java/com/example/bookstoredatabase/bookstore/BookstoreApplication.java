28565d6ac38f3f6b548f10ba7619afa86b855c44
Wanbro <48619709+Wanbro@users.noreply.github.com>
2019-09-22 18:59:10 +0300

    Bookstore: Spring Data REST
    Enabled Spring REST services

--- bookstore/src/main/java/com/example/bookstoredatabase/bookstore/BookstoreApplication.java
+++ bookstore/src/main/java/com/example/bookstoredatabase/bookstore/BookstoreApplication.java
@@ -1,8 +1,5 @@
 package com.example.bookstoredatabase.bookstore;
 
-import org.slf4j.Logger;
-
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,13 +7,12 @@ import org.springframework.context.annotation.Bean;
 
 import com.example.bookstoredatabase.bookstore.domain.Book;
 import com.example.bookstoredatabase.bookstore.domain.BookRepository;
-import com.example.bookstoredatabase.bookstore.domain.CategoryRepository;
 import com.example.bookstoredatabase.bookstore.domain.Category;
-
+import com.example.bookstoredatabase.bookstore.domain.CategoryRepository;
 
 @SpringBootApplication
 public class BookstoreApplication {
-	
+
 	public static void main(String[] args) {
 		SpringApplication.run(BookstoreApplication.class, args);
 	}
@@ -24,17 +20,14 @@ public class BookstoreApplication {
 	@Bean
 	public CommandLineRunner demo(BookRepository repository, CategoryRepository crepository) {
 		return (args) -> {
-			
-			
+
 			crepository.save(new Category("Kauhu"));
 			crepository.save(new Category("Romantiikka"));
 			crepository.save(new Category("Kauno"));
-			
+
 			repository.save(new Book("Aleksanteri", "Jaskan talvi", "1992", crepository.findByName("Kauhu").get(0)));
 			repository.save(new Book("Matti", "Maalaan taivasta", "1993", crepository.findByName("Kauno").get(0)));
-			
-			
-			
+
 		};
 	}
 }
