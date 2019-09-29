a2adb7232612b20b98ecf0a50bda65b61e7dea34
Aleksanteri Fagerholm <48619709+Wanbro@users.noreply.github.com>
2019-09-29 20:10:13 +0300

    Bookstore: Securing the web application
    Added spring security along the lines with a login/logout page that has been separated to admin and user status. Admin and user have different rights.

--- bookstore/src/main/java/com/example/bookstoredatabase/bookstore/BookstoreApplication.java
+++ bookstore/src/main/java/com/example/bookstoredatabase/bookstore/BookstoreApplication.java
@@ -1,5 +1,8 @@
 package com.example.bookstoredatabase.bookstore;
 
+import org.slf4j.Logger;
+
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,9 +13,13 @@ import com.example.bookstoredatabase.bookstore.domain.BookRepository;
 import com.example.bookstoredatabase.bookstore.domain.Category;
 import com.example.bookstoredatabase.bookstore.domain.CategoryRepository;
 
-@SpringBootApplication
-public class BookstoreApplication {
 
+@SpringBootApplication
+
+
+public class BookstoreApplication {
+	private static final Logger log = LoggerFactory.getLogger(BookstoreApplication.class);
+	
 	public static void main(String[] args) {
 		SpringApplication.run(BookstoreApplication.class, args);
 	}
@@ -27,7 +34,9 @@ public class BookstoreApplication {
 
 			repository.save(new Book("Aleksanteri", "Jaskan talvi", "1992", crepository.findByName("Kauhu").get(0)));
 			repository.save(new Book("Matti", "Maalaan taivasta", "1993", crepository.findByName("Kauno").get(0)));
-
+			
+			
+			
 		};
 	}
 }
